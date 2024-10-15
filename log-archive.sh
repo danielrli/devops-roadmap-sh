f4d8275c692677b44e9cb055db40d1ce6865d06e
Daniel <danielrli92@gmail.com>
2024-10-15 10:45:46 -0700

    changed to argument from prompt

--- log-archive.sh
+++ log-archive.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-read -r -p "Specify Log Directory: " input
-input="${input:-/var/log}"
+# Prompt User for log directory
+#read -r -p "Specify Log Directory: " input
+#input="${input:-/var/log}"
+
+# Pass Argument into script
+if [ -z "$1" ]; then
+    echo "Specify a log directory: log-archive <log-dir>"
+    exit 1
+fi
+
+input="$1"
 
 if [ ! -d "$input" ]; then
     echo "Error: Log directory does not exist."
@@ -19,11 +28,11 @@ if ! mkdir -p "$archive_directory"; then
     exit 1
 fi
 
-#Make time stamp and set file name
+# Make time stamp and set file name
 timestamp=$(date +"%Y%m%d_%H%M%S")
 archive_file="$archive_directory/logs_archive_$timestamp.tar.gz"
 
-#Change working directory to $input to flatten
+# Change working directory to $input to flatten
 (cd "$input" && tar --exclude="./archive" -czvf "$archive_file" ./*)
 
 echo "Logs archvied at: $archive_file"
