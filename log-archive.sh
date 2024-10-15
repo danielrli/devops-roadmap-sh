eef1a214fbd31843fdcd1f2c0f59b8983c4d08f5
Daniel <danielrli92@gmail.com>
2024-10-15 10:36:22 -0700

    completed log-archive

--- log-archive.sh
+++ log-archive.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
 read -r -p "Specify Log Directory: " input
-echo "$input"
+input="${input:-/var/log}"
 
+if [ ! -d "$input" ]; then
+    echo "Error: Log directory does not exist."
+    input=""
+    exit 1
+fi
+
+echo "Log directory set to $input"
+
+archive_directory="$input/archive"
+mkdir "$archive_directory"
+
+if ! mkdir -p "$archive_directory"; then
+    echo "Error creating $archive_directory."
+    exit 1
+fi
+
+#Make time stamp and set file name
+timestamp=$(date +"%Y%m%d_%H%M%S")
+archive_file="$archive_directory/logs_archive_$timestamp.tar.gz"
+
+#Change working directory to $input to flatten
+(cd "$input" && tar --exclude="./archive" -czvf "$archive_file" ./*)
+
+echo "Logs archvied at: $archive_file"
